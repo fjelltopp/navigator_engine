51385d10507653132d173f7f2b2af93d33b3b15a
Jonathan Berry <jonathan.s.berry@googlemail.com>
2021-10-25 12:15:01 +0100

    Get working with minikube

---
+++ README.md
@@ -0,0 +1,5 @@
+# HIV Tools Navigator
+## The Engine
+
+The Navigator Engine process and manages decision logic to determine what should happen next
+to a given data structure.
--- navigator_engine/app.py
+++ navigator_engine/app.py
@@ -1,7 +1,8 @@
-from flask import Flask
-from raven.contrib.flask import Sentry
 import os
+import sentry_sdk
+from flask import Flask
 from flask_restful import Api
+from sentry_sdk.integrations.flask import FlaskIntegration
 from navigator_engine.api_resources.whats_next import WhatsNext
 from navigator_engine.api_resources.decision_graph import DecisionGraph
 from navigator_engine.api_resources.conditional import Conditional
@@ -13,10 +14,13 @@ config_object = os.getenv('CONFIG_OBJECT', 'navigator_engine.config.Development'
 app.config.from_object(config_object)
 app.config.from_envvar('NAVIGATOR_ENGINE_SETTINGS', silent=True)
 
-if app.config["SENTRY_DNS"]:
-    sentry = Sentry(app, dsn=app.config["SENTRY_DNS"])
-else:
-    sentry = None
+if app.config.get("SENTRY_DSN"):
+    sentry_sdk.init(
+        dsn=app.config["SENTRY_DSN"],
+        integrations=[FlaskIntegration()],
+        traces_sample_rate=1.0
+    )
+
 
 api = Api(app)
 api.add_resource(WhatsNext, "/whatsnext")
