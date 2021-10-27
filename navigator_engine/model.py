a5d7389b42b43526658cd733e8b97474cfea3a8a
Jonathan Berry <jonathan.s.berry@googlemail.com>
2021-10-27 11:11:14 +0100

    NAV-22 Initial draft of data model

--- navigator_engine/model.py
+++ navigator_engine/model.py
@@ -2,3 +2,61 @@ from flask_sqlalchemy import SQLAlchemy
 import networkx
 
 db = SQLAlchemy()
+
+
+class Graph(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    version = db.Column(db.String, nullable=False)
+    description = db.Column(db.String)
+    nodes = db.relationship("Node", back_populates="graph")
+    edges = db.relationship("Edge", back_populates="graph")
+
+    def to_networkx(self):
+        network = networkx.DiGraph()
+        network.add_edges_from([edge.tuple() for edge in self.edges])
+        self.network = network
+        return network
+
+
+class Conditional(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    function = db.Column(db.String)
+    nodes = db.relationship("Node", back_populates="conditional")
+
+
+class Action(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    html = db.Column(db.String)
+    skippable = db.Column(db.Boolean, default=False)
+    action_url = db.Column(db.String)
+    nodes = db.relationship("Node", back_populates="action")
+
+
+class Node(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    graph_id = db.Column(db.Integer,  db.ForeignKey('graph.id'), nullable=False)
+    conditional_id = db.Column(db.Integer, db.ForeignKey('conditional.id'))
+    action_id = db.Column(db.Integer, db.ForeignKey('action.id'))
+    action = db.relationship("Action", back_populates="nodes")
+    conditional = db.relationship("Conditional", back_populates="nodes")
+    graph = db.relationship("Graph", back_populates="nodes")
+
+    def __hash__(self):
+        return self.id
+
+
+class Edge(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    graph_id = db.Column(db.Integer,  db.ForeignKey('graph.id'), nullable=False)
+    from_id = db.Column(db.Integer, db.ForeignKey('node.id'), nullable=False)
+    to_id = db.Column(db.Integer, db.ForeignKey('node.id'), nullable=False)
+    type = db.Column(db.Boolean, nullable=False)
+    graph = db.relationship("Graph", back_populates="edges")
+    from_node = db.relationship("Node", foreign_keys=[from_id])
+    to_node = db.relationship("Node", foreign_keys=[to_id])
+
+    def tuple(self):
+        return (self.from_node, self.to_node, {'type': self.type, 'object': self})
