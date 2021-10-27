dd8d9ea95c6189eec371c7c6115a28a0f64f72cd
Jonathan Berry <jonathan.s.berry@googlemail.com>
2021-10-27 11:04:28 +0100

    NAV-31 Improve config.

--- navigator_engine/config.py
+++ navigator_engine/config.py
@@ -1,17 +1,19 @@
+import logging
+
+
 class Config(object):
     DEBUG = False
     TESTING = False
     PRODUCTION = False
-
-
-class Production(Config):
-    PRODUCTION = True
-
-
-class Development(Config):
-    DEBUG = True
-    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1234567890@db/engine'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    LOGGING_LEVEL = logging.INFO
 
 
 class Testing(Config):
     TESTING = True
+
+
+class Production(Config):
+    PRODUCTION = True
+    LOGGING_LEVEL = logging.WARNING
