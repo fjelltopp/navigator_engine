77644098441140aee06edc4a4767f3661b521ed5
Jyri Soppela <jyri@fjelltopp.org>
2021-11-02 08:56:00 +0200

    NAV-38 move graph_loader import into beginning of file

--- navigator_engine/app.py
+++ navigator_engine/app.py
@@ -4,6 +4,7 @@ from flask import Flask
 from sentry_sdk.integrations.flask import FlaskIntegration
 from navigator_engine.model import db
 from navigator_engine.api import api
+from navigator_engine.graph_loader import graph_loader
 
 
 def create_app(config_object=None):
@@ -27,7 +28,6 @@ def create_app(config_object=None):
     db.init_app(app)
     api.init_app(app)
 
-    from navigator_engine.graph_loader import graph_loader
     with app.app_context():
         graph_loader()
 
