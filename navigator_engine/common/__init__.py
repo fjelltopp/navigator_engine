d8d8bc02822b061f1b4f8365c1211a2d2a529af0
Jonathan Berry <jonathan.s.berry@googlemail.com>
2021-11-03 17:42:25 +0000

    NAV-40 PEP change

--- navigator_engine/common/__init__.py
+++ navigator_engine/common/__init__.py
@@ -9,6 +9,7 @@ def register_conditional(f):
 def register_loader(f):
     DATA_LOADERS[f.__name__] = f
 
+
 class DecisionError(Exception):
     """Raised when there is an error in the decision logic."""
     pass
