66fc3dc82864b3bc7aedddca5853aab1eb50fc00
Jonathan Berry <jonathan.s.berry@googlemail.com>
2021-11-05 16:53:52 +0000

    NAV-40 Simpler graph loading.

--- navigator_engine/tests/integration_tests/test_graph_processing.py
+++ navigator_engine/tests/integration_tests/test_graph_processing.py
@@ -6,11 +6,11 @@ import navigator_engine.tests.util as test_util
 
 
 @pytest.mark.parametrize("data, expected_node_id", [
-    ({1: False, 2: True, 3: True, 4: True}, 5),
-    ({1: True, 2: False, 3: True, 4: True}, 6),
+    ({1: False, 2: True, 3: True, 4: True}, 3),
+    ({1: True, 2: False, 3: True, 4: True}, 5),
     ({1: True, 2: True, 3: False, 4: True}, 7),
-    ({1: True, 2: True, 3: True, 4: False}, 8),
-    ({1: True, 2: True, 3: True, 4: True}, 9),
+    ({1: True, 2: True, 3: True, 4: False}, 9),
+    ({1: True, 2: True, 3: True, 4: True}, 8),
 ])
 @pytest.mark.usefixtures('with_app_context')
 def test_graph_processing(data, expected_node_id):
@@ -22,8 +22,8 @@ def test_graph_processing(data, expected_node_id):
 
 
 @pytest.mark.parametrize("data, skip_steps, expected_node_id", [
-    ({1: True, 2: False, 3: False, 4: True}, [6], 7),
-    ({1: True, 2: False, 3: False, 4: False}, [6, 7], 8),
+    ({1: True, 2: False, 3: False, 4: True}, [5], 7),
+    ({1: True, 2: False, 3: False, 4: False}, [5, 7], 9),
 ])
 @pytest.mark.usefixtures('with_app_context')
 def test_with_skip_steps(data, skip_steps, expected_node_id):
@@ -36,8 +36,8 @@ def test_with_skip_steps(data, skip_steps, expected_node_id):
 
 
 @pytest.mark.parametrize("data, skip_steps", [
-    ({1: False, 2: False, 3: False, 4: False}, [5]),
-    ({1: True, 2: True, 3: True, 4: True}, [9])
+    ({1: False, 2: False, 3: False, 4: False}, [3]),
+    ({1: True, 2: True, 3: True, 4: True}, [8])
 ])
 @pytest.mark.usefixtures('with_app_context')
 def test_skipping_unskippable_step_raises_error(data, skip_steps):
@@ -50,7 +50,7 @@ def test_skipping_unskippable_step_raises_error(data, skip_steps):
 
 @pytest.mark.parametrize("data, expected_node_id", [
     ({1: True, 2: True, 'data': {1: True, 2: True, 3: False, 4: True}}, 7),
-    ({1: True, 2: True, 'data': {1: False, 2: True, 3: True, 4: True}}, 5),
+    ({1: True, 2: True, 'data': {1: False, 2: True, 3: True, 4: True}}, 3),
     ({1: True, 2: True, 'data': {1: True, 2: True, 3: True, 4: True}}, 15)
 ])
 @pytest.mark.usefixtures('with_app_context')
