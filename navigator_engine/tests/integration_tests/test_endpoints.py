9eb5dd652ce746e2e640e8f4904600482de2561c
Jonathan Berry <jonathan.s.berry@googlemail.com>
2021-11-08 17:50:09 +0000

    NAV-54 decide endpoint test stub

--- navigator_engine/tests/integration_tests/test_endpoints.py
+++ navigator_engine/tests/integration_tests/test_endpoints.py
@@ -1,3 +1,6 @@
+import json
+import pytest
+import navigator_engine.tests.util as test_util
 """
 Endpoint tests use the client fixture, which requires the db, meaning they should
 be treated as integration tests.
@@ -8,3 +11,9 @@ def test_index(client):
     response = client.get("/")
     assert response.status_code == 200
     assert response.data == b"Navigator Engine Running"
+
+
+@pytest.mark.usefixtures('with_app_context')
+def test_decide(client):
+    test_util.create_demo_data()
+    response = client.post("/api/decide", data=json.dumps({'test': 'data'}))
