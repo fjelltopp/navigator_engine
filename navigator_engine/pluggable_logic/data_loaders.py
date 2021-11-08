caa5bb60c3f493705eb16485df5226f5845f30a3
Jonathan Berry <jonathan.s.berry@googlemail.com>
2021-11-08 17:54:57 +0000

    NAV-53 JSON data loader

--- navigator_engine/pluggable_logic/data_loaders.py
+++ navigator_engine/pluggable_logic/data_loaders.py
@@ -1,4 +1,6 @@
 from navigator_engine.common import register_loader
+import requests
+import json
 
 
 @register_loader
@@ -9,3 +11,14 @@ def return_empty(data):
 @register_loader
 def dict_value(key, data):
     return data[key]
+
+
+@register_loader
+def json_url(url_key, authorization_header_key, data):
+    url = data[url_key]
+    headers = f"Authorization: {data[authorization_header_key]}"
+    response = requests.get(url, headers=headers)
+    loaded_data = json.loads(response.data)
+    loaded_data['navigator_engine_source_url'] = url
+    loaded_data['navigator_engine_auth_headers'] = headers
+    return loaded_data
