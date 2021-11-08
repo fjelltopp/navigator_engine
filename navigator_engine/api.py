407e45c7e43dc51a6e0b80518f80cbf48c8f6412
Jonathan Berry <jonathan.s.berry@googlemail.com>
2021-11-08 16:34:16 +0000

    NAV-43 New API design

--- navigator_engine/api.py
+++ navigator_engine/api.py
@@ -1,30 +1,42 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify
 
 api_blueprint = Blueprint('main', __name__, url_prefix='/api/')
 
 
-@api_blueprint.route('/decision-engine/create', methods=['POST'])
-def decision_engine_create():
-    return jsonify({"message": "success"})
+@api_blueprint.route('/decide', methods=['POST'])
+def decide():
+    """
+    Decide what needs to happen next for a given data file.
 
+    POST Request takes the following json input:
+    ```
+        {
+            "data": {
+                "url": "<url from estimates dataset json datadict>",
+                "authorization_header": "<optional value to be supplied as the Authorization header tag>"
+            },
+            "skip_actions": ["<action_id>", "<action_id>"]
+        }
+    ```
+    """
 
-@api_blueprint.route('/decision-engine/<engine_id>/progress')
-def decision_engine_progress(engine_id):
     return jsonify({
-        "overallProgress": 35,
-        "milestoneListFullyResolved": False,
-        "milestones":
-            [
+        "actions": ["<action_id>", "<action_id>", "<action_id>", "<action_id>"],
+        "skipped": ["<action_id>", "<action_id>"],
+        "progress": {
+            "overallProgress": 35,
+            "milestoneListFullyResolved": False,
+            "milestones": [
                 {
                     "id": "xxx",
                     "title": "Naomi Data Prep",
                     "completed": True,
                     "progress": 100
                 }, {
-                "id": "yyy",
-                "title": "Shiny 90 Data Prep",
-                "completed": False,
-                "progress": 50
+                    "id": "yyy",
+                    "title": "Shiny 90 Data Prep",
+                    "completed": False,
+                    "progress": 50
                 }, {
                     "id": "zzz",
                     "title": "Update Spectrum",
@@ -32,42 +44,26 @@ def decision_engine_progress(engine_id):
                     "progress": 0
                 }
             ]
-    })
-
-
-@api_blueprint.route('/decision-engine/<engine_id>/action-path')
-def decision_engine_action_path(engine_id):
-    return jsonify({
-        "actions":
-            ["<action_id>", "<action_id>", "<action_id>", "<action_id>"]
-    })
-
-
-@api_blueprint.route('/decision-engine/<engine_id>/decide')
-def decision_engine_decide(engine_id):
-    _skip_actions = request.args.get('skip-actions', default='')
-    if len(_skip_actions) == 0:
-        skip_actions = []
-    else:
-        skip_actions = _skip_actions.split(',')
-
-    return jsonify({
-        "id": "xxx",
-        "skipped": False,
-        "content": {
-            "title": "Node Title",
-            "display_html": "<p>Display HTML</p>",
-            "skippable": True,
-            "action_url": "http://fjelltopp.org"
+        },
+        "decision": {
+            "id": "xxx",
+            "content": {
+                "title": "Node Title",
+                "display_html": "<p>Lorem Ipsum</p>",
+                "skippable": True,
+                "action_url": "http://fjelltopp.org"
+            }
         }
     })
 
 
-@api_blueprint.route('/decision-engine/<engine_id>/action/<action_id>')
-def decision_engine_action(engine_id, action_id):
+@api_blueprint.route('/action/<action_id>')
+def action(action_id):
+    """
+    Return the contents of a particular action.
+    """
     return jsonify({
         "id": "xxx",
-        "skipped": True,
         "content": {
             "title": "Node Title",
             "display_html": "<p>Lorem Ipsum</p>",
