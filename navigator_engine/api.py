97fd8d1f29a12bf9b17c83f788b5dbef93e1305c
Jonathan Berry <jonathan.s.berry@googlemail.com>
2021-11-08 17:49:44 +0000

    NAV-54 Draft of decide endpoint

--- navigator_engine/api.py
+++ navigator_engine/api.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from navigator_engine.common.decision_engine import DecisionEngine
+from navigator_engine.model import load_graph
+from navigator_engine.common import DATA_LOADERS
+import json
 
 api_blueprint = Blueprint('main', __name__, url_prefix='/api/')
 
@@ -19,41 +23,22 @@ def decide():
         }
     ```
     """
-    return jsonify({
-        "actions": ["<action_id>", "<action_id>", "<action_id>", "<action_id>"],
-        "skippedActions": ["<action_id>", "<action_id>"],
-        "progress": {
-            "overallProgress": 35,
-            "milestoneListFullyResolved": False,
-            "milestones": [
-                {
-                    "id": "xxx",
-                    "title": "Naomi Data Prep",
-                    "completed": True,
-                    "progress": 100
-                }, {
-                    "id": "yyy",
-                    "title": "Shiny 90 Data Prep",
-                    "completed": False,
-                    "progress": 50
-                }, {
-                    "id": "zzz",
-                    "title": "Update Spectrum",
-                    "completed": False,
-                    "progress": 0
-                }
-            ]
-        },
-        "decision": {
-            "id": "xxx",
-            "content": {
-                "title": "Node Title",
-                "displayHTML": "<p>Lorem Ipsum</p>",
-                "skippable": True,
-                "actionURL": "http://fjelltopp.org"
-            }
-        }
-    })
+    input_data = json.loads(request.data)
+
+    graph = load_graph(1)
+    data = DATA_LOADERS['json_url']('url', 'authorization_header', input_data['data'])
+    skip_actions = input_data.get('skipActions', [])
+
+    engine = DecisionEngine(graph, data, skip=skip_actions)
+    engine.decide()
+
+    result = {
+        "decision": engine.decision,
+        "actions": engine.progress.action_breadcrumbs(),
+        "skipped": engine.progress.skipped,
+        "progress": engine.progress.report_progress(),
+    }
+    return result
 
 
 @api_blueprint.route('/action/<action_id>')
