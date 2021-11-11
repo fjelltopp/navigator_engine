7aa04cd003396dfcf420b7ea27c1c225e112075a
Jonathan Berry <jonathan.s.berry@googlemail.com>
2021-11-11 22:11:48 +0000

    NAV-68 Typing for pluggable logic

--- navigator_engine/pluggable_logic/conditional_functions.py
+++ navigator_engine/pluggable_logic/conditional_functions.py
@@ -1,30 +1,32 @@
 from navigator_engine.common import register_conditional
+from navigator_engine.common.decision_engine import DecisionEngine
+from typing import Hashable
 
 
 @register_conditional
-def return_true(engine):
+def return_true(engine: DecisionEngine) -> bool:
     return True
 
 
 @register_conditional
-def return_false(engine):
+def return_false(engine: DecisionEngine) -> bool:
     return False
 
 
 @register_conditional
-def dict_value(key, engine):
+def dict_value(key: Hashable, engine: DecisionEngine) -> bool:
     return bool(engine.data[key])
 
 
 @register_conditional
-def check_dict_value(key, value, engine):
+def check_dict_value(key: Hashable, value: Hashable, engine: DecisionEngine) -> bool:
     return engine.data[key] == value
 
 
 @register_conditional
-def check_not_skipped(actions, engine):
+def check_not_skipped(actions: list[int], engine: DecisionEngine) -> bool:
     if type(actions) is not list:
-        raise TypeError(f"Must specify a list of node ids instead of {actions}")
+        raise TypeError(f"Must specify a list of node IDs instead of {actions}")
     skipped_actions = set(actions) & set(engine.progress.skipped)
     engine.remove_skips += list(skipped_actions)
     return not bool(skipped_actions)
