f7a7eb9a6640e515e36e0278fc4820ffbcfe1810
Jyri Soppela <jyri@fjelltopp.org>
2021-11-11 12:56:51 +0200

    NAV-58 fix graph update

--- navigator_engine/graph_viz.py
+++ navigator_engine/graph_viz.py
@@ -15,26 +15,24 @@ def get_dash_app(flask_app, dash_app):
     dash_app.layout = html.Div(children=[
         html.Div([
             "Input: ",
-            dcc.Input(id='graph-selector', value='type id of graph', type='number')
+            dcc.Input(id='graph-selector', value=1, type='number')
         ]),
         html.Div(
-            id='cytoscape',
+            id='cytoscape-container',
             children=[
                 cyto.Cytoscape(
                     id='cytoscape-figure',
                     layout={'name': 'preset'},
                     style={'width': '100%', 'height': '400px'},
                     elements=[
-                        {'data': {'id': 'one', 'label': 'Node 1'}, 'position': {'x': 75, 'y': 75}},
-                        {'data': {'id': 'two', 'label': 'Node 2'}, 'position': {'x': 200, 'y': 200}},
-                        {'data': {'source': 'one', 'target': 'two'}}
+                        {'data': {'id': 'nav', 'label': 'Navigator Engine'}, 'position': {'x': 75, 'y': 75}}
                     ])
             ]
         )
     ])
 
     @dash_app.callback(
-        Output(component_id='cytoscape', component_property='children'),
+        Output(component_id='cytoscape-container', component_property='children'),
         Input(component_id='graph-selector', component_property='value'))
     def update_figure(graph_id):
 
