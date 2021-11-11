c97f4bbcd942cae45142bfebfc9e7f9863583802
Jonathan Berry <jonathan.s.berry@googlemail.com>
2021-11-11 22:11:48 +0000

    NAV-68 Typing for pluggable logic

--- navigator_engine/pluggable_logic/data_loaders.py
+++ navigator_engine/pluggable_logic/data_loaders.py
@@ -1,20 +1,22 @@
 from navigator_engine.common import register_loader
 import requests
-import json
+from typing import Hashable
+from navigator_engine.common.decision_engine import DecisionEngine
 
 
 @register_loader
-def return_empty(engine):
+def return_empty(engine: DecisionEngine) -> dict:
     return {}
 
 
 @register_loader
-def dict_value(key, engine):
+def dict_value(key: Hashable, engine: DecisionEngine) -> Hashable:
     return engine.data[key]
 
 
 @register_loader
-def json_url(url_key, authorization_header_key, engine):
+def json_url(url_key: Hashable,
+             authorization_header_key: Hashable, engine: DecisionEngine) -> dict:
     url = engine.data[url_key]
     headers = {"Authorization": engine.data[authorization_header_key]}
     response = requests.get(url, headers=headers)
