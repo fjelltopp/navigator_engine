c30763dab1152bbc6d713a1712a076128167608a
Jyri Soppela <jyri@fjelltopp.org>
2021-11-12 14:35:19 +0200

    NAV-58 refactor graph_visualizer into common folder

--- navigator_engine/common/dash_app.py
+++ navigator_engine/common/dash_app.py
@@ -1,11 +1,11 @@
 import dash
 import flask
-from navigator_engine import graph_viz
+from navigator_engine.common import graph_visualizer
 
 
 def add_dash_app(navigator_app):
     dash_app = dash.Dash(__name__, server=navigator_app, url_base_pathname='/graph/')
-    dash_app = graph_viz.get_dash_app(flask_app=navigator_app, dash_app=dash_app)
+    dash_app = graph_visualizer.get_dash_app(flask_app=navigator_app, dash_app=dash_app)
 
     @navigator_app.route('/graph')
     def graph():
