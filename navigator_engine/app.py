6fd7f2d58f5ee1f0bb5d77c6419c58cd798b3201
Jyri Soppela <jyri@fjelltopp.org>
2021-11-12 14:03:40 +0200

    NAV-58 refer to main navigator flask app as app

--- navigator_engine/app.py
+++ navigator_engine/app.py
@@ -18,7 +18,7 @@ import json
 
 def create_app(config_object=None):
 
-    flask_app = Flask(__name__)
+    app = Flask(__name__)
 
     if not config_object:
         config_object = os.environ.get(
@@ -26,30 +26,28 @@ def create_app(config_object=None):
             'navigator_engine.config.Config'
         )
 
+    app.config.from_object(config_object)
+    app.url_map.strict_slashes = False
+    app.logger.setLevel(app.config.get('LOGGING_LEVEL'))
 
-    flask_app.config.from_object(config_object)
-    flask_app.config.from_envvar('NAVIGATOR_ENGINE_SETTINGS', silent=True)
-    flask_app.url_map.strict_slashes = False
-    flask_app.logger.setLevel(flask_app.config.get('LOGGING_LEVEL'))
-
-    if flask_app.config.get("SENTRY_DSN"):
+    if app.config.get("SENTRY_DSN"):
         sentry_sdk.init(
-            dsn=flask_app.config["SENTRY_DSN"],
+            dsn=app.config["SENTRY_DSN"],
             integrations=[FlaskIntegration()],
             traces_sample_rate=1.0
         )
 
-    db.init_app(flask_app)
-    flask_app.register_blueprint(api_blueprint)
-    cli.register(flask_app)
+    db.init_app(app)
+    app.register_blueprint(api_blueprint)
+    cli.register(app)
 
-    dash_app = dash.Dash(__name__, server=flask_app, url_base_pathname='/graph/')
-    dash_app = graph_viz.get_dash_app(flask_app=flask_app, dash_app=dash_app)
+    dash_app = dash.Dash(__name__, server=app, url_base_pathname='/graph/')
+    dash_app = graph_viz.get_dash_app(flask_app=app, dash_app=dash_app)
 
     # Importing this code registers all the pluggable_logic for use
     importlib.import_module('navigator_engine.pluggable_logic')
 
-    @flask_app.route('/')
+    @app.route('/')
     def index():
         return jsonify({"status": "Navigator Engine Running"})
 
@@ -64,11 +62,11 @@ def create_app(config_object=None):
         response.content_type = "application/json"
         return response
 
-    @flask_app.route('/graph')
+    @app.route('/graph')
     def graph():
         return flask.redirect('/graph')
 
-    return flask_app
+    return app
 
 
 app = create_app()
