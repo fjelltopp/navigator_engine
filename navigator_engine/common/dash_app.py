671448f9858dd5e1bf2729cda0fdd9afbd8e0a84
Jyri Soppela <jyri@fjelltopp.org>
2021-11-15 15:15:09 +0200

    NAV-58 add type hint to dash app adder

--- navigator_engine/common/dash_app.py
+++ navigator_engine/common/dash_app.py
@@ -3,7 +3,7 @@ import flask
 from navigator_engine.common import graph_visualizer
 
 
-def add_dash_app(navigator_app):
+def add_dash_app(navigator_app) -> flask.app.Flask:
     dash_app = dash.Dash(__name__, server=navigator_app, url_base_pathname='/graph/')
     graph_visualizer.create_visualizer(flask_app=navigator_app, dash_app=dash_app)
 
