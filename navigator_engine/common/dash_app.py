404ba4278bca7c2d6c2c4d2f5453e779eb6a9959
Tomasz Sabała <tomek@fjelltopp.org>
2021-11-16 15:49:42 +0100

    Fixing routing for the dash app.

--- navigator_engine/common/dash_app.py
+++ navigator_engine/common/dash_app.py
@@ -4,7 +4,10 @@ from navigator_engine.common import graph_visualizer
 
 
 def add_dash_app(navigator_app) -> flask.app.Flask:
-    dash_app = dash.Dash(__name__, server=navigator_app, url_base_pathname='/graph/')
+    dash_app = dash.Dash(__name__,
+                         server=navigator_app,
+                         requests_pathname_prefix='/engine/graph/',
+                         routes_pathname_prefix='/graph/')
     graph_visualizer.create_visualizer(flask_app=navigator_app, dash_app=dash_app)
 
     return navigator_app
