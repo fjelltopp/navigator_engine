887ee0036789c9ad17cc301a47f15df6ce19023a
Jyri Soppela <jyri@fjelltopp.org>
2021-11-22 15:18:42 +0200

    NAV-60 always use same excel for end to end tests

--- navigator_engine/tests/integration_tests/test_end_to_end.py
+++ navigator_engine/tests/integration_tests/test_end_to_end.py
@@ -1,5 +1,4 @@
 from navigator_engine.common.graph_loader import graph_loader, validate_graph
-from navigator_engine.tests.util import app
 import pytest
 import json
 import os
@@ -8,7 +7,8 @@ import os
 @pytest.mark.vcr()
 @pytest.mark.usefixtures('with_app_context')
 def test_end_to_end(client):
-    graph_loader('Estimates 22 BDG [Final].xlsx')
+    base_directory = os.path.abspath(os.path.dirname(__file__))
+    graph_loader(f'{base_directory}/../test_data/Estimates 22 BDG [Final].xlsx')
     validate_graph(1)
     # For the time being the following code is ignored
     # It will be updated once the production graph is loading properly
