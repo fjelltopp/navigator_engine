b0466937e9b7f52a6de0cbd8235c20da3912f0be
Jyri Soppela <jyri@fjelltopp.org>
2021-11-25 13:16:10 +0200

    NAV-140 simplify handling of empty data set
    Co-authored-by: Jonathan Berry <jonathan.s.berry@gmail.com>

--- navigator_engine/pluggable_logic/data_loaders.py
+++ navigator_engine/pluggable_logic/data_loaders.py
@@ -100,26 +100,12 @@ def load_csv_from_zipped_resource(resource_type: str,
                                   name: str,
                                   engine: DecisionEngine) -> dict:
 
-    try:
-        data = load_estimates_dataset_resource(resource_type, auth_header, engine)
-        if not data[resource_type]['url'] \
-                and not data[resource_type]['auth_header'] \
-                and not data[resource_type]['data']:
-            raise ValueError(f"Empty resource {resource_type} in {data[resource_type]['url']}")
-    except IOError:
-        data = {
-            name: {
-                'data': None,
-                'auth_header': None,
-                'url': None
-            }
-        }
-        return data
-    except ValueError:
+    data = load_estimates_dataset_resource(resource_type, auth_header, engine)
+    if not data[resource_type]['data']:
         data[name] = {
             'data': None,
-            'auth_header': data[resource_type].get('auth_header'),
-            'url': data[resource_type].get('url')
+            'auth_header': None,
+            'url': None
         }
         return data
 
