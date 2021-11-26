0c4636a1cd19b2ba65f107e7e692d260058afef8
Jyri Soppela <jyri@fjelltopp.org>
2021-11-26 10:10:32 +0200

    NAV-152 fix file reference

--- navigator_engine/tests/unit_tests/test_conditionals.py
+++ navigator_engine/tests/unit_tests/test_conditionals.py
@@ -91,15 +91,15 @@ def test_check_dataset_valid(resources, expected, mock_engine):
 
 @pytest.mark.parametrize("checklist, dataframe, expected, raises_error", [
     (['Adult male ART has 2020 data', 'Ped VS as 2020 data', 'Adult ART coverage never exceeds 100%'],
-     pd.read_csv('../test_data/test_spectrum_check.csv'), True, does_not_raise()),
+     pd.read_csv('tests/test_data/test_spectrum_check.csv'), True, does_not_raise()),
     (['Uncertainty analysis is valid', 'Adult off ART mortality is default'],
-     pd.read_csv('../test_data/test_spectrum_check.csv'), False, does_not_raise()),
+     pd.read_csv('tests/test_data/test_spectrum_check.csv'), False, does_not_raise()),
     (['Adult male ART has 2020 data', 'Ped VS as 2020 data', 'Adult ART coverage never exceeds 100%'],
      None, False, does_not_raise()),
     (['Adult male ART has 2020 data', 'Adult progression is default', 'Shiny90 is valid'],
-     pd.read_csv('../test_data/test_spectrum_check.csv'), True, does_not_raise()),
+     pd.read_csv('tests/test_data/test_spectrum_check.csv'), True, does_not_raise()),
     (['Adult male ART has 2020 data', 'Ped VS as 2020 data', 'This indicator does not exist'],
-     pd.read_csv('../test_data/test_spectrum_check.csv'), None, pytest.raises(DecisionError))
+     pd.read_csv('tests/test_data/test_spectrum_check.csv'), None, pytest.raises(DecisionError))
 ])
 def test_check_spectrum_file(checklist, dataframe, expected, raises_error, mock_engine):
 
