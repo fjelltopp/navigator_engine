5af2a2f8c21413571a2b5559a17445bdb2976758
Jyri Soppela <jyri@fjelltopp.org>
2021-12-02 09:47:00 +0200

    NAV-193 update test parameters

--- navigator_engine/tests/unit_tests/test_conditionals.py
+++ navigator_engine/tests/unit_tests/test_conditionals.py
@@ -98,13 +98,13 @@ def test_check_dataset_valid(resources, expected, mock_engine):
 
 
 @pytest.mark.parametrize("checklist, dataframe, expected, raises_error", [
-    (['Adult male ART has 2020 data', 'Ped VS as 2020 data', 'Adult ART coverage never exceeds 100%'],
+    (['Adult male ART has 2020 data', 'Adult ART coverage never exceeds 100%'],
      pd.read_csv('navigator_engine/tests/test_data/test_spectrum_check.csv'), True, does_not_raise()),
     (['Uncertainty analysis is valid', 'Adult off ART mortality is default'],
      pd.read_csv('navigator_engine/tests/test_data/test_spectrum_check.csv'), False, does_not_raise()),
     (['Adult male ART has 2020 data', 'Ped VS as 2020 data', 'Adult ART coverage never exceeds 100%'],
      None, False, does_not_raise()),
-    (['Adult male ART has 2020 data', 'Adult progression is default', 'Shiny90 is valid'],
+    (['Adult male ART has 2020 data', 'Adult progression is default'],
      pd.read_csv('navigator_engine/tests/test_data/test_spectrum_check.csv'), True, does_not_raise()),
     (['Adult male ART has 2020 data', 'Ped VS as 2020 data', 'This indicator does not exist'],
      pd.read_csv('navigator_engine/tests/test_data/test_spectrum_check.csv'), None, pytest.raises(DecisionError))
