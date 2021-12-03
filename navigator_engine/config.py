67c8d317f26e084a263d889e6cdbb7d0f312bd89
Tomek Sabała <sabala.tomek@gmail.com>
2021-12-03 10:12:43 +0100

    NAV-201 Introducing sentry integration (#76)
    * NAV-201 Introducing sentry integration
    
    * Fixup.
    
    * Fixup read env type from flask config.

--- navigator_engine/config.py
+++ navigator_engine/config.py
@@ -4,12 +4,14 @@ base_directory = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
+    ENV_TYPE = os.getenv("ENV_TYPE")
     DEBUG = False
     TESTING = False
     PRODUCTION = False
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1234567890@db/navigator_engine'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     LOGGING_LEVEL = logging.INFO
+    SENTRY_DSN = os.getenv("SENTRY_DSN")
     DASH_REROUTE_PREFIX = '/engine'
     DEFAULT_DECISION_GRAPH = f'{base_directory}/../Estimates 22 BDG [Final].xlsx'
 
