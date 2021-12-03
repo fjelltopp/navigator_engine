a20f87c94c072afa702fd6e88dcb360d12276d8c
Jyri Soppela <jyri@fjelltopp.org>
2021-12-03 11:38:41 +0200

    NAV-198 fix mixup between Naomi and Spectum files

--- navigator_engine/pluggable_logic/conditional_functions.py
+++ navigator_engine/pluggable_logic/conditional_functions.py
@@ -76,16 +76,16 @@ def check_spectrum_file(indicators: list[str], engine: DecisionEngine) -> bool:
     if checklist is None:
         return False
     for indicator in indicators:
-        if indicator not in checklist['NaomiCheckPermPrimKey'].values:
+        if indicator not in checklist['ID'].values:
             raise navigator_engine.common.DecisionError(
-                f'Indicator "{indicator}" not found in Naomi checklist'
+                f'Indicator "{indicator}" not found in Spectrum checklist'
             )
-    indicator_checks = checklist[checklist['NaomiCheckPermPrimKey'].isin(indicators)]
-    indicator_checks['TrueFalse'].replace([0, 'FALSE', 'F', 'false', 'f'],
+    indicator_checks = checklist[checklist['ID'].isin(indicators)]
+    indicator_checks['Status'].replace([0, 'FALSE', 'F', 'false', 'f'],
                                           value=False,
                                           inplace=True)
 
-    indicator_checks['TrueFalse'][indicator_checks['TrueFalse'].ne(False)] = True
+    indicator_checks['Status'][indicator_checks['Status'].ne(False)] = True
 
     return indicator_checks['Status'].eq(True).all()
 
@@ -98,14 +98,17 @@ def check_naomi_file(indicators: list[str], engine: DecisionEngine) -> bool:
         return False
 
     for indicator in indicators:
-        if indicator not in checklist['ID'].values:
+        if indicator not in checklist['NaomiCheckPermPrimKey'].values:
             raise navigator_engine.common.DecisionError(
-                f'Indicator "{indicator}" not found in Spectrum checklist'
+                f'Indicator "{indicator}" not found in Naomi checklist'
             )
-    indicator_checks = checklist[checklist['ID'].isin(indicators)]
-    indicator_checks['Status'] = indicator_checks['Status'].replace(
-        [0, 'FALSE', 'F', 'false', 'f'],
-        value=False,
-    )
-    indicator_checks['Status'][indicator_checks['Status'].ne(False)] = True
-    return indicator_checks['Status'].eq(True).all()
+
+    indicator_checks = checklist[checklist['NaomiCheckPermPrimKey'].isin(indicators)]
+    indicator_checks['TrueFalse'].replace([0, 'FALSE', 'F', 'false', 'f'],
+                                          value=False,
+                                          inplace=True)
+
+    indicator_checks['TrueFalse'][indicator_checks['TrueFalse'].ne(False)] = True
+
+    return indicator_checks['TrueFalse'].eq(True).all()
+
