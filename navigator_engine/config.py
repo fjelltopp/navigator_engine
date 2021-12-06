fc71cdc937519a2337dc5298d96f43792334cfe9
Michał <fulior@users.noreply.github.com>
2021-12-06 09:43:09 +0100

    NAV-159 Added JSON logger (#74)
    * Added JSON logger
    
    * Ignore json_logging type checks.
    
    Co-authored-by: Tomasz Sabała <tomek@fjelltopp.org>

--- navigator_engine/config.py
+++ navigator_engine/config.py
@@ -8,6 +8,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     PRODUCTION = False
+    JSON_LOGGING = bool(os.getenv("JSON_LOGGING", False))
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1234567890@db/navigator_engine'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     LOGGING_LEVEL = logging.INFO
@@ -34,3 +35,4 @@ class Production(Config):
     PRODUCTION = True
     SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI", "not-set")
     LOGGING_LEVEL = logging.ERROR
+    JSON_LOGGING = bool(os.getenv("JSON_LOGGING", True))
