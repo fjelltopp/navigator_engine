619e0409c09ac200ba7a520ef35375bbed55c93b
Michal Ful <michal@fjelltopp.org>
2021-12-06 13:37:44 +0100

    Fix JSON logging toggle

--- navigator_engine/config.py
+++ navigator_engine/config.py
@@ -8,7 +8,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     PRODUCTION = False
-    JSON_LOGGING = bool(os.getenv("JSON_LOGGING", False))
+    JSON_LOGGING = (os.getenv("JSON_LOGGING", 'false').lower() == 'true')
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1234567890@db/navigator_engine'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     LOGGING_LEVEL = logging.INFO
@@ -35,4 +35,4 @@ class Production(Config):
     PRODUCTION = True
     SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI", "not-set")
     LOGGING_LEVEL = logging.ERROR
-    JSON_LOGGING = bool(os.getenv("JSON_LOGGING", True))
+    JSON_LOGGING = (os.getenv("JSON_LOGGING", 'true').lower() == 'true')
