a6183a58d837ea13140786dfa93104a80cea262f
Jonathan Berry <jonathan.s.berry@googlemail.com>
2021-12-13 16:50:07 +0000

    NAV-211 Read new workflow state structure.

--- navigator_engine/pluggable_logic/conditional_functions.py
+++ navigator_engine/pluggable_logic/conditional_functions.py
@@ -1,7 +1,7 @@
 import navigator_engine.common
 from navigator_engine.common import register_conditional, get_resource_from_dataset
 from navigator_engine.common.decision_engine import DecisionEngine
-from typing import Hashable, Union
+from typing import Hashable, Union, Any
 import re
 
 
@@ -27,46 +27,54 @@ def check_dict_value(key: Hashable, value: Hashable, engine: DecisionEngine) -> 
 
 @register_conditional
 def check_not_skipped(actions: list[str], engine: DecisionEngine) -> bool:
+
     if type(actions) is not list:
         raise TypeError(f"Must specify an list of node IDs instead of {actions}")
+
     skipped_actions = [action for action in actions if action in engine.progress.skipped_actions]
     engine.remove_skip_requests += list(skipped_actions)
+
     return not bool(skipped_actions)
 
 
 @register_conditional
 def check_manual_confirmation(action_id: int, engine: DecisionEngine) -> bool:
-    workflow_state = engine.data['navigator-workflow-state'].get('data')
-    if workflow_state:
-        completed_tasks = workflow_state.get('completedTasks', [])
-    else:
-        completed_tasks = []
-    return action_id in completed_tasks
+    return action_id in _get_completed_actions(engine)
 
 
 @register_conditional
 def check_resource_key(resource_types: Union[list[str], str], key: Hashable, value: Hashable, engine: DecisionEngine) -> bool:
-    dataset = engine.data['dataset']['data']['result']
+
     if type(resource_types) is str:
         resource_types = [resource_types]
+
+    dataset = engine.data['dataset']['data']['result']
     results = []
+
     for resource_type in resource_types:
         resource = get_resource_from_dataset(resource_type, dataset)
+
         if type(value) is str and type(resource.get(key)) is str:
             regex = re.compile(value)
             match_result = bool(regex.match(resource.get(key, "")))
         else:
             match_result = resource.get(key) == value
+
         results.append(match_result)
+
     return all(results)
 
 
 @register_conditional
 def check_dataset_valid(engine: DecisionEngine) -> bool:
+
     dataset = engine.data['dataset']['data']['result']
+
     for resource in dataset.get('resources', []):
+
         if resource.get('validation_status', 'success') != 'success':
             return False
+
     return True
 
 
@@ -92,21 +100,38 @@ def check_naomi_file(indicators: list[str], engine: DecisionEngine) -> bool:
     )
 
 
-def _check_validation_file(indicators: list[str], data_source: str, id_column: str,
-                           result_column: str, engine: DecisionEngine) -> bool:
+def _check_validation_file(indicators: list[str], data_source: str,
+                           id_column: str, result_column: str, engine: DecisionEngine) -> bool:
+
     checklist = engine.data[data_source]['data']
+
     if checklist is None:
         return False
+
     indicators = [indicator.lower() for indicator in indicators]
+
     for indicator in indicators:
+
         if indicator not in checklist[id_column].str.lower().values:
             raise navigator_engine.common.DecisionError(
                 f'Indicator "{indicator}" not found in {data_source}'
             )
+
     indicator_checks = checklist[checklist[id_column].str.lower().isin(indicators)]
     indicator_results = indicator_checks[result_column].replace(
         [0, 'FALSE', 'F', 'false', 'f'],
         value=False
     )
     indicator_results[indicator_results.ne(False)] = True
+
     return indicator_results.eq(True).all()
+
+
+def _get_completed_actions(engine: DecisionEngine) -> dict[str, dict[str, Any]]:
+
+    workflow_state = engine.data['navigator-workflow-state'].get('data')
+
+    if not workflow_state:
+        return {}
+    else:
+        return {task['id']: task for task in workflow_state.get('completedTasks', [])}
--- navigator_engine/tests/integration_tests/test_end_to_end.py
+++ navigator_engine/tests/integration_tests/test_end_to_end.py
@@ -4,11 +4,11 @@ import os
 
 
 @pytest.mark.vcr()
-def test_end_to_end(test_production_client):
+def test_estimates_start(test_production_client):
     response = test_production_client.post("/api/decide", data=json.dumps({
         'data': {
             'url': 'https://dev.adr.fjelltopp.org/api/3/action/package_show'
-                   '?id=antarctica-country-estimates-2022-1-2-3',
+                   '?id=czechia-country-estimates-2022',
             'authorization_header': os.getenv('ADR_SYSADMIN_KEY')
         },
         'skipActions': ['EST-OVV-01-10-A']
@@ -19,25 +19,11 @@ def test_end_to_end(test_production_client):
 
 
 @pytest.mark.vcr()
-def test_spectrum_file_check(test_production_client):
-    response = test_production_client.post("/api/decide", data=json.dumps({
-        'data': {
-            'url': 'https://dev.adr.fjelltopp.org/api/3/action/package_show'
-                   '?id=slovakia-country-estimates-2022',
-            'authorization_header': os.getenv('ADR_SYSADMIN_KEY')
-        }
-    }))
-
-    assert response.status_code == 200
-    assert response.json['decision']['id'] == 'EST-SPF-16-01-A'
-
-
-@pytest.mark.vcr()
 def test_estimates_complete(test_production_client):
     response = test_production_client.post("/api/decide", data=json.dumps({
         'data': {
             'url': 'https://dev.adr.fjelltopp.org/api/3/action/package_show'
-                   '?id=vanuatu-country-estimates-2022',
+                   '?id=slovakia-country-estimates-2022',
             'authorization_header': os.getenv('ADR_SYSADMIN_KEY')
         }
     }))
