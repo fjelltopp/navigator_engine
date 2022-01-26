7c9756e8d261edeae133c6c457e5880a781ccf97
Jyri Soppela <jyri@fjelltopp.org>
2022-01-26 14:11:55 +0200

    nav-254 bdg string extractor

--- navigator_engine/common/bdg_message_extractor.py
+++ navigator_engine/common/bdg_message_extractor.py
@@ -4,20 +4,16 @@ import re
 
 MILESTONE_COLUMNS = {
     'TITLE': 'Milestone Title (Visible to User):',
-    'VERSION': 'Version:',
     'DESCRIPTION': 'Milestone Description (Visible to User):',
     'COMPLETE_MESSAGE': 'Complete Message',
-    'DATA_LOADER': 'Data Loader:'
+    'ENTRY_CRITERIA': 'Entry Criteria:'
 }
 
 DATA_COLUMNS = {
     'TITLE': 'Task Test (if test passes, proceed to next test)',
     'ACTION': 'Task Title (Visible to User)',
     'ACTION_CONTENT': 'If test fails, present this to user:',
-    'ACTION_RESOURCES': 'Resources / Links',
-    'UNSKIPPABLE': 'Mandatory right now',
-    'SKIP_TO': 'Proceed to test (if test fails)',
-    'FUNCTION': 'Test function'
+    'ACTION_RESOURCES': 'Resources / Links'
 }
 
 
@@ -39,7 +35,7 @@ def extract_bdg(fileobj, keywords=None, comment_tags=None, options=None):
     #xl = pd.ExcelFile(app.config.get('DEFAULT_DECISION_GRAPH'))
     regex = re.compile(r'[\d]{2,2}-')
     graph_sheets = list(filter(lambda x: regex.match(x), xl.sheet_names))
-    graphs = {}
+    messages = []
 
     for index, sheet_name in enumerate(graph_sheets):
 
@@ -49,6 +45,9 @@ def extract_bdg(fileobj, keywords=None, comment_tags=None, options=None):
             header=0
         ).loc[0][0:7]
 
+        for key in MILESTONE_COLUMNS:
+            messages.append([1, None, graph_header.loc[MILESTONE_COLUMNS[key]], None])
+
         graph_data = pd.read_excel(
             fileobj,
             sheet_name=sheet_name,
@@ -56,12 +55,15 @@ def extract_bdg(fileobj, keywords=None, comment_tags=None, options=None):
             index_col=0,
             dtype=str
         )
-        print('foo')
+
+        for graph_data_index, row in graph_data.iterrows():
+            for key in DATA_COLUMNS:
+                messages.append([1, None, row.loc[DATA_COLUMNS[key]], None])
+
+    for message in messages:
+        yield tuple(message)
 
 
 with open(app.config.get('DEFAULT_DECISION_GRAPH'), 'rb') as f:
     extract_bdg(f)
 
-    #for cell in _get_cells(xl):
-    #    yield (message_lineno, funcname, messages,
-    #                           [comment[1] for comment in translator_comments])
