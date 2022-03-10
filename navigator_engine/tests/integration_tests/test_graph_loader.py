c62b7c0a329340ea3adcf2ca0bd4a2c57ca56ce9
Jyri Soppela <jyri@fjelltopp.org>
2022-03-10 22:47:15 +0200

    NAV-254 format code

--- navigator_engine/tests/integration_tests/test_graph_loader.py
+++ navigator_engine/tests/integration_tests/test_graph_loader.py
@@ -47,15 +47,15 @@ class TestGraphLoader:
             assert networkx.algorithms.isomorphism.is_isomorphic(graph.to_networkx(), graph_test_data[str(graph.id)])
 
     @pytest.mark.parametrize('language,description', [
-        ('en', 'In this milestone, you will prepare and place the required input data in the templates needed to ' +
-               'generate HIV estimates. You should use the best, highest quality, and most complete input data you ' +
-               'have available to you for the estimates process.'),
-        ('fr', 'In this milestone, you will prepare and place the required input data in the templates needed to ' +
-               'generate HIV estimates. You should use the best, highest quality, and most complete input data you ' +
-               'have available to you for the estimates process.'),
-        ('pt', 'In this milestone, you will prepare and place the required input data in the templates needed to ' +
-               'generate HIV estimates. You should use the best, highest quality, and most complete input data you ' +
-               'have available to you for the estimates process.')
+        ('en', 'In this milestone, you will prepare and place the required input data in the templates needed to '
+               + 'generate HIV estimates. You should use the best, highest quality, and most complete input data you '
+               + 'have available to you for the estimates process.'),
+        ('fr', 'In this milestone, you will prepare and place the required input data in the templates needed to '
+               + 'generate HIV estimates. You should use the best, highest quality, and most complete input data you '
+               + 'have available to you for the estimates process.'),
+        ('pt', 'In this milestone, you will prepare and place the required input data in the templates needed to '
+               + 'generate HIV estimates. You should use the best, highest quality, and most complete input data you '
+               + 'have available to you for the estimates process.')
     ])
     def test_graph_translation(self, mocker, language, description):
         mocker.patch('navigator_engine.model.get_locale', return_value=language)
@@ -64,9 +64,12 @@ class TestGraphLoader:
 
     def test_conditional_translation(self):
         conditionals = model.Node.query.all()
+        assert conditionals
 
     def test_action_translation(self):
         actions = model.Action.query.all()
+        assert actions
 
     def test_milestone_translation(self):
         milestones = model.Milestone.query.all()
+        assert milestones
